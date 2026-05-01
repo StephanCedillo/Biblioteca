2afbc9c1bc8f45a8f10d984df96943b2c04c2c68
Alfonso Auquilla <alfonsoauquilla180@gmail.com>
2026-05-01 17:30:57 -0500

    primer commit

---
+++ OneDrive/Documentos/NetBeansProjects/Biblioteca1 - Copy/src/main/java/ec/edu/ups/tareagrupal/clases/Membresia.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.ups.tareagrupal.clases;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author stephancedillo
+ */
+public class Membresia {
+
+    String tipoMembresia;
+    LocalDate fechaInicio;
+    LocalDate fechaVencimiento;
+    LocalDate hoy = LocalDate.now();
+
+    public Membresia() {
+    }
+
+    public Membresia(String tipoMembresia, LocalDate fechaInicio, LocalDate fechaVencimiento) {
+        this.tipoMembresia = tipoMembresia;
+        this.fechaInicio = fechaInicio;
+        this.fechaVencimiento = fechaVencimiento;
+    }
+
+    public String getTipoMembresia() {
+        return tipoMembresia;
+    }
+
+    public void setTipoMembresia(String tipoMembresia) {
+        this.tipoMembresia = tipoMembresia;
+    }
+
+    public LocalDate getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(LocalDate fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public LocalDate getFechaVencimiento() {
+        return fechaVencimiento;
+    }
+
+    public void setFechaVencimiento(LocalDate fechaVencimiento) {
+        this.fechaVencimiento = fechaVencimiento;
+    }
+
+    @Override
+    public String toString() {
+        return "Membresia{" + "tipoMembresia=" + tipoMembresia + ", fechaInicio=" + fechaInicio + ", fechaVencimiento=" + fechaVencimiento + '}';
+    }
+
+    public int calcularDescuento() {
+        if (tipoMembresia.equalsIgnoreCase("Corporativa")) {
+            return 25;
+        } else if (tipoMembresia.equalsIgnoreCase("Academica")) {
+            return 15;
+        } else if (tipoMembresia.equalsIgnoreCase("Estudiantil")) {
+            return 20;
+        } else {
+            return 0;
+        }
+    }
+
+    public boolean estaVigente() {
+        return fechaVencimiento.isBefore(hoy);
+    }
+
+    public void renovar() {
+        fechaInicio = hoy;
+        fechaVencimiento = hoy;
+        fechaVencimiento.plusMonths(3);
+    }
+
+    public String obtenerDiasRestantes() {
+        int mesesRestantes = fechaVencimiento.getMonthValue() - fechaInicio.getMonthValue();
+        int diasRestantes = fechaVencimiento.getDayOfMonth() - fechaInicio.getDayOfMonth();
+        return "Los meses restantes son : " + mesesRestantes + " y los dias faltantes : " + diasRestantes + " ";
+    }
+
+}
---
+++ OneDrive/Documentos/NetBeansProjects/Biblioteca1 - Copy/src/main/java/ec/udu/ups/tareagrupal/biblioteca/Biblioteca.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.udu.ups.tareagrupal.biblioteca;
+
+/**
+ *
+ * @author stephancedillo
+ */
+public class Biblioteca {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
